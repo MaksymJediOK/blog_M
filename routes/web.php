933b7bb7899ef6f84334c2517dbfeab37075773d
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2022-12-09 12:06:38 +0200

    Lab3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RestTestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
